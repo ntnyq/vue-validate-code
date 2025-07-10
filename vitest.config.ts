85aa7550dca936f385f304bcaf06cd0a2770654d
ntnyq <ntnyq13@gmail.com>
2025-07-10 12:04:55 +0800

    test: cleanup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,11 +7,6 @@ export default defineConfig({
     environment: 'jsdom',
     reporters: ['dot'],
     setupFiles: ['./vitest.setup.ts'],
-    coverage: {
-      all: false,
-      include: ['src/**/*.ts', 'src/**/*.vue'],
-      reporter: ['lcov', 'text'],
-    },
     environmentOptions: {
       jsdom: {
         resources: 'usable',
